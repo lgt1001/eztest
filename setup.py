3748348dbaf08d5e1507ca460d74fa05d8ea9283
bealiu <bealiu@cisco.com>
2018-06-30 00:11:01 -0700

    only support python2.7 and python3.2+, add 3rd package: psutil.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 Copyright (C) 2014 lgt, version 1.0 by lgt
 * https://github.com/lgt1001/eztest
 """
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="eztest",
@@ -20,7 +20,12 @@ setup(
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
@@ -29,10 +34,11 @@ setup(
         "Topic :: Software Development :: Libraries",
         "Intended Audience :: Developers",
     ],
+    install_requires=['psutil'],
     keywords='performance load test testing performance-test load-test performance-testing load-testing',
     license="GPL",
     platforms=["Windows", "MacOS", "Unix", "Linux"],
-    packages=find_packages('eztest', exclude=['test']),
+    packages=['eztest'],
     zip_safe=False,
     entry_points=dict(console_scripts=['eztest=eztest:main'])
 )
